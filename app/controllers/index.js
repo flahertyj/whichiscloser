d32c97eee155bfc0801914d0dc96ee084c4530be
John Flaherty <flahertyj@gmail.com>
2014-07-24 23:32:59 -0600

    Initial commit of Titanium project.

---
+++ app/controllers/index.js
@@ -0,0 +1,79 @@
+var MapModule = require('ti.map');
+var mapview = MapModule.createView({mapType:MapModule.NORMAL_TYPE});
+
+var rc = MapModule.isGooglePlayServicesAvailable();
+switch (rc) {
+    case MapModule.SUCCESS:
+        Ti.API.info('Google Play services is installed.');
+        break;
+    case MapModule.SERVICE_MISSING:
+        alert('Google Play services is missing. Please install Google Play services from the Google Play store.');
+        break;
+    case MapModule.SERVICE_VERSION_UPDATE_REQUIRED:
+        alert('Google Play services is out of date. Please update Google Play services.');
+        break;
+    case MapModule.SERVICE_DISABLED:
+        alert('Google Play services is disabled. Please enable Google Play services.');
+        break;
+    case MapModule.SERVICE_INVALID:
+        alert('Google Play services cannot be authenticated. Reinstall Google Play services.');
+        break;
+    default:
+        alert('Unknown error.');
+        break;
+}
+
+var win = Ti.UI.createWindow({backgroundColor: 'white'});
+
+var map1 = MapModule.createView({
+    userLocation: true,
+    mapType: MapModule.NORMAL_TYPE,
+    animate: true,
+    region: {latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 },
+    height: '50%',
+    top: 0,
+    left: 0,
+    width: '50%'
+});
+
+var map2 = MapModule.createView({
+    userLocation: true,
+    mapType: MapModule.TERRAIN_TYPE,
+    animate: true,
+    region: {latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 },
+    height: '50%',
+    top: 0,
+    right: 0,
+    width: '50%'
+});
+
+var map3 = MapModule.createView({
+    userLocation: true,
+    mapType: MapModule.SATELLITE_TYPE,
+    animate: true,
+    region: {latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 },
+    height: '50%',
+    bottom: 0,
+	left: 0,
+    width: '50%'
+});
+
+var map4 = MapModule.createView({
+    userLocation: true,
+    mapType: MapModule.HYBRID_TYPE,
+    animate: true,
+    region: {latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 },
+    height: '50%',
+    bottom: 0,
+    right: 0,
+    width: '50%',
+    traffic: true
+});
+
+win.add(map1);
+win.add(map2);
+win.add(map3);
+win.add(map4);
+win.open();
+
+$.index.open();
